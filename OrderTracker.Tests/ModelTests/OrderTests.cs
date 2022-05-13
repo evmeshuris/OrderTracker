6a13549d9f4ec1b54550974edd1fe6c31fead8c4
Evgeniya Meshuris <em@theemm.com>
2022-05-13 14:49:46 -0700

    it will add tests that checks if VendorName is a string; VendorConstructor; if it returns AllVendors

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -20,6 +20,47 @@ namespace OrderTracker.Tests
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
-    
+    [TestMethod]
+    public void OrderDescription_ReturnDescription_String()
+    {
+      string description = "brownie";
+      int price01 = 1;
+      Order testOrder = new Order(description, price01);
+      string result = testOrder.Description;
+
+      Assert.AreEqual(description, result);
+    }
+
+    [TestMethod]
+    public void SetDescription_SetDescription_String()
+    {
+      string description = "cookies";
+      int price01 = 1;
+      Order newOrder = new Order(description, price01);
+      string updatedDescription = "cake";
+      newOrder.Description = updatedDescription;
+      string result = newOrder.Description;
+      Assert.AreEqual(updatedDescription, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_OrderList()
+    {
+      List<Order> newList = new List<Order> { };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+    [TestMethod]
+    public void GetAll_ReturnsOrders_OrderList()
+    {
+      string description01 = "brownies";
+      string description02 = "cookies";
+      int price01 = 1;
+      int price02 = 2;
+      Order newOrder01 = new Order(description01, price01);
+      Order newOrder02 = new Order(description02, price02);
+      List<Order> newList = new List<Order> { newOrder01, newOrder02 };
+      List<Order> result = Order.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
   }
 }
--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -18,6 +18,15 @@ namespace OrderTracker.Tests
       Vendor newVendor = new Vendor("test vendor");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
     }
+    [TestMethod]
+    public void GetVendorName_ReturnVendorName_String()
+    {
+      string vendorName = "Pierre's Bakery";
+      Vendor testVendor = new Vendor(vendorName);
+      string result = testVendor.VendorName;
+
+      Assert.AreEqual(vendorName, result);
+    }
     
-   }
+    }
 }
